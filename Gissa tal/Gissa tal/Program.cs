8bdae2756f8b295bd0c21255a85c98fb6538b52a
Ida <idsp03001@utb.vaxjo.se>
2020-11-25 19:22:27 +0100

    Skrivit tre variabler och en WriteLine

---
+++ Gissa tal/Gissa tal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gissa_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal;
+            int räknare;
+            int gissning;
+            Console.WriteLine("Ange ett heltal mellan 1 och 100");
+
+        }
+    }
+}
