d4e890202fb8b36024e833e7aa54e8cb52bca865
Ida <idsp03001@utb.vaxjo.se>
2020-11-26 21:34:46 +0100

    Lagt till några if-satser

--- Gissa tal/Gissa tal/Program.cs
+++ Gissa tal/Gissa tal/Program.cs
@@ -10,10 +10,20 @@ namespace Gissa_tal
     {
         static void Main(string[] args)
         {
-            int tal;
-            int räknare;
-            int gissning;
-            Console.WriteLine("Ange ett heltal mellan 1 och 100");
+            int tal; //det talet som slumpas fram
+            int räknare; //räknare för att hålla koll på antalet gissningar som har gjorts
+            Console.WriteLine("Ange ett heltal mellan 1 och 100"); //fråga till användaren
+            string strgissning = Console.ReadLine();
+            int gissning = Convert.ToInt32(strgissning);
+
+            if (gissning < tal) //om gissningen är mindre än det slumpade talet
+                Console.WriteLine("Gissningen är för låg");
+
+            if (gissning > tal) //om gissningen är högre än det slumpade talet
+                Console.WriteLine("Gissningen är för hög");
+
+            if (gissning == tal) //om gissningen är lika med det slumpade talet
+                Console.WriteLine("Gissningen är rätt!");
 
         }
     }
